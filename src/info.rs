0a3ae45fee44c69fdc684eae0cdd0c418bf1fd61
Brenden <brenden.ferguson@gmail.com>
2022-02-15 19:38:25 -0800

    add commandline instructions when errors are encountered or ran without arguments

---
+++ src/info.rs
@@ -0,0 +1,17 @@
+use std::process;
+
+pub fn print_info(){
+    let string ="Name: chargeto\n \
+    Synopsis: sudo chargeto 60\n \
+    Description:\n \
+        chargeto is a tool to control the maximum charge level of a laptop battery on linux devices.\n \
+        It requires linux kernel 5.4+ and systemd.\n \
+        Optional arguments: \n \
+            -no-systemd : Skip installation of chargeto systemd service\n \
+            -uninstall-systemd : Uninstall chargeto systemd service";
+    println!("{}", string);
+
+
+    
+    process::exit(1);
+}
